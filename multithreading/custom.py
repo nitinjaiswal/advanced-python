2c7d8c10a1eb194b14390ccddf44f0fd2c3ee032
nitinjaiswal <nitingreat04@gmail.com>
2017-02-12 18:01:45 +0530

    multithreading

---
+++ multithreading/custom.py
@@ -0,0 +1,32 @@
+import threading
+import time
+
+
+class MyThread(threading.Thread):
+    def __init__(self, name, repeat):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.repeat = repeat
+
+    def run(self):
+        print(self.name + " starts")
+        inc = 1
+        while inc <= self.repeat:
+            time.sleep(4)
+            print(self.name + " : executes " + str(inc))
+            inc += 1
+        print(self.name + " ends")
+
+
+def main():
+    thread1 = MyThread("Thread1", 3)
+    thread2 = MyThread("Thread2", 4)
+    thread1.start()
+    thread2.start()
+    print(thread1.getName())
+    thread2.getName()
+
+    print("Main complete")
+
+if __name__ == "__main__":
+    main()
---
+++ multithreading/multithreading.py
@@ -0,0 +1,24 @@
+from threading import Thread
+import time
+
+
+def task(name, repeat):
+    print(name + " starts")
+    inc = 1
+    while inc <= repeat:
+        time.sleep(4)
+        print(name + " : execution " + str(inc))
+        inc += 1
+    print(name + " ends")
+
+
+def main():
+    thread1 = Thread(target=task, args=("Thread1", 3))
+    thread2 = Thread(target=task, args=("Thread2", 4))
+    thread1.start()
+    thread2.start()
+
+    print("Main complete")
+
+if __name__ == "__main__":
+    main()
